a286e0e0243e372116f4699900387669391fb89b
Kris Foster <krisfoster@Kriss-MacBook-Air.local>
2018-12-29 14:59:07 +0000

    bug fix

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_mudules/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
